02ed1d7d60ff2dfb933b6394dd3cb53ea07731b2
Won Song <wosong@microsoft.com>
2023-09-12 11:36:13 -0400

    Add conversation tracking

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, send_from_directory, request
 from modules.open_ai import OpenAI
+from modules.message_history import MessageHistory
 import json
+from uuid import uuid4
 
 app = Flask(__name__)
+message_history = MessageHistory()
 
 
 @app.route('/')
@@ -13,9 +16,21 @@ def home():
 @app.route('/ask', methods=['POST'])
 def build_model():
     payload = json.loads(request.data)
-    response = OpenAI(payload['account'], payload['model']).ask(prompt=payload['prompt'], key=payload['apikey'])
+    conversation_id = payload['conversationId'] if payload['conversationId'] is not None else uuid4()
 
-    return response
+    message_history.append_user_message(conversation_id, payload['prompt'])
+    messages = message_history.get_messages(conversation_id)
+
+    try:
+        response = OpenAI(payload['account'], payload['model'], payload['apikey']).complete(messages)
+        message_history.append_assistant_message(conversation_id, response)
+
+        return {
+            'conversationId': conversation_id,
+            'response': response
+        }
+    except Exception as ex:
+        return str(ex), 500
 
 
 @app.route('/<path:path>')
--- modules/open_ai.py
+++ modules/open_ai.py
@@ -5,36 +5,34 @@ import json
 class OpenAI():
     base_url = 'https://%s.azure-api.net/openai/deployments/%s/chat/completions?api-version=2023-07-01-preview'
 
-    def __init__(self, account, deployment_name):
+    def __init__(self, account, deployment_name, key):
         self.account = account
         self.deployment_name = deployment_name
+        self.key = key
 
-    def ask(self, prompt, key):
+    def complete(self, messages, temperature=0.7, top_p=0.95, frequency_penalty=0, presence_penalty=0, max_tokens=2048,
+                 stop=None):
         response = requests.request(
             'post',
             self.base_url % (self.account, self.deployment_name),
             headers={
-                "api-key": f"{key}",
+                "api-key": f"{self.key}",
                 "Content-Type": "application/json"
             },
             data=json.dumps({
-                "messages": [
-                    {
-                        "role": "user",
-                        "content": f"{prompt}"
-                    }
-                ],
-                "temperature": 0.7,
-                "top_p": 0.95,
-                "frequency_penalty": 0,
-                "presence_penalty": 0,
-                "max_tokens": 2048,
-                "stop": None
+                "messages": messages,
+                "temperature": temperature,
+                "top_p": top_p,
+                "frequency_penalty": frequency_penalty,
+                "presence_penalty": presence_penalty,
+                "max_tokens": max_tokens,
+                "stop": stop
             })
         )
 
         json_obj = json.loads(response.text)
-        # TODO: Need to do more validation on the responses
-        message = json_obj['choices'][0]['message']['content']
+        if response.status_code is not 200:
+            raise Exception(json_obj['message'])
 
+        message = json_obj['choices'][0]['message']['content']
         return message
