b1341538e68be58d13fdc027675af7ec7da76c00
Carlos Rodriguez <carodriguez@microsoft.com>
2023-09-13 14:18:43 -0400

    Added the JSON parsing method (still needs work to when handling ** at the end of the sentence)

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import json
 from uuid import uuid4
 from config import Config
 
+import re
+
 app = Flask(__name__)
 message_history = MessageHistory()
 
@@ -34,6 +36,9 @@ def build_model():
         response = OpenAI(Config.instance, Config.model, Config.apiKey).complete(messages)
         message_history.append_assistant_message(conversation_id, response)
 
+        if "**Outcome:**" in response or "**Scenario:**" in response or "**Options**" in response:
+            parse_gpt_response(response)
+
         return {
             'conversationId': conversation_id,
             'response': response,
@@ -46,6 +51,37 @@ def build_model():
 @app.route('/<path:path>')
 def static_file(path):
     return app.send_static_file(path)
+
+# This private method should probably live in its own file
+# Method works but sometimes GPT will return the syntax
+# **Outcome: this is the outcome** and when it does
+# logic goes out of whack. 
+def parse_gpt_response(response):
+    
+    splitted_body = re.split('(\*\*.*?\*\*)', response)
+    print(splitted_body)
+    outcome_idx = splitted_body.index("**Outcome:**")+1
+    scenario_idx = splitted_body.index("**Scenario:**")+1
+    options_idx = splitted_body.index("**Options:**")+1
+
+    outcome_str = splitted_body[outcome_idx]
+    scenario_str = splitted_body[scenario_idx]
+    options_str = splitted_body[options_idx]
+
+    print("Outcome: " + outcome_str)
+    print("Scenario: " + scenario_str)
+    print("Options: " + options_str)
+
+    splitted_options = re.split('([1-9]\.)', options_str)
+
+    options_1 = splitted_options.index("1.")+1
+    options_2 = splitted_options.index("2.")+1
+    options_3 = splitted_options.index("3.")+1
+
+    options = [splitted_options[options_1],splitted_options[options_2],splitted_options[options_3]]
+    json_body = {"outcome": outcome_str,"scenario": scenario_str, "options":options }
+    
+    return json_body
 
 
 if __name__ == '__main__':
