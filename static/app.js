7e6d564e2be9cdc21d19c0108a07e14d273f5e59
gferioli <gferioli@microsoft.com>
2023-09-14 15:53:40 -0400

    make changes to frontend

--- static/app.js
+++ static/app.js
@@ -1,31 +1,85 @@
+let preprompt = "Assume the role of a game with extensive knowledge on career experiences, essential life skills, interactive fiction writing, and narrating life experiences through stories while I play the role of the player. Your task is to help me, a person with mental disabilities, by helping me gain useful career skillsets and essential life skills through different scenarios by creating an interactive story. Within your narrated stories, provide suitable names for characters, locations, groups and organizations, events, and items that are connected to the career that the player chooses. The game will begin with me providing a career of my choosing that I would like to pursue and you will then lead the game by providing various scenarios or experiences that I will experience within that career or career field.\nOnce I have provided my career title, you will provide one scenario per chat and each scenario teaches me a skill that I will need within the chosen career or an essential life skill. Some experiences can include, but are not limited to, role responsibilities within the chosen career, difficult conversations, financial literacy, and other work experiences.\nFor the scenario responses, provide three multiple-choice options around how I can react or respond to the scenario or experience that you provided. I will then choose one of the provided options and you will then provide an in-depth narrative that describes what will happen next in the story along with the next scenario and three new multiple-choice options that I can select from. Each game will be 10 scenarios long with a recap at the end that gives an overview of lessons learned."
+let conversationId = ""
+
+
+let TTsEnabled = false
 //Toggle prompt and game divs
 let toggle = button => {
-    let element = document.getElementById("prompt-center");
-    if (element.style.display === "none") {
-        element.style.display = "block";
-    } else {
-        element.style.display = "none";
-    }
-
-    let gameElement = document.getElementById("game-center");
-    if (gameElement.style.display === "none") {
-        gameElement.style.display = "block";
-    } else {
-        gameElement.style.display = "none";
-    }
+  let element = document.getElementById("prompt-center");
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
   }
+  let gameElement = document.getElementById("game-center");
+  if (gameElement.style.display === "none") {
+    gameElement.style.display = "block";
+  } else {
+    gameElement.style.display = "none";
+  }
+}
 
 //send post requests
-let postCareer = prompt => {
-    //post logic here
+let postCareer = async () => {
+  var careerInput = document.getElementById("career");
+  var optionButton1 = document.getElementById("option1");
+  var optionButton2 = document.getElementById("option2");
+  var optionButton3 = document.getElementById("option3");
+  console.log("start")
+  console.log(careerInput)
+  try {
+
+    let body = {
+      conversationId: conversationId, system_prompt: preprompt, user_response: careerInput.value, prompt: "{0} display your response in markdown syntax for outcome, scenario, and options"
+    }
+
+    // Make an HTTP POST request to the API
+    const response = await fetch('http://127.0.0.1:8080/ask', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+
+    // Check if the response status is OK (200)
+    if (!response.ok) {
+      throw new Error('Failed to post career data to the API');
+    }
+
+    // Parse the response data as JSON
+    const data = await response.json();
+
+
+    // Now you can work with the response data
+    console.log('Response from the API:', data);
+
+    conversationId = data['conversationId']
+
+    console.log(conversationId)
+
+    // options picked up here
+    optionButton1.textContent = "option1 from chatgpt"
+    optionButton2.textContent = "option2 from chatgpt"
+    optionButton3.textContent = "option3 from chatgpt"
+
+    console.log(optionButton1)
+
+    // You can return the data or do further processing here
+    return data;
+  } catch (error) {
+    console.error('Error posting career data:', error);
+    throw error;
+  }
 }
 
 let postButton = optionNum => {
-    //post logic here
+  //post logic here
 }
 
 let toggleTTS = () => {
-    //TTS logic here
+  //TTS logic here
+  TTsEnabled = True
 }
 
 
@@ -48,7 +102,7 @@ window.addEventListener('mousemove', updtMouse);
 
 var dots = new Array();
 
-var Dot = function(x, y, dx, dy) {
+var Dot = function (x, y, dx, dy) {
   this.x = x;
   this.y = y;
   this.dx = dx;
@@ -58,19 +112,19 @@ var Dot = function(x, y, dx, dy) {
 function updtMouse(e) {
   mx = e.x;
   my = e.y;
-  console.log(mx + " " + my);
+
 }
 
 function init() {
 
-  for(let i=0; i<dots_num; i++) {
-    let x = Math.floor((Math.random()*innerWidth/uni_divs)+(parseInt(i/(dots_num/uni_divs))*(innerWidth/uni_divs)));
-    let y = Math.floor(Math.random()*innerHeight);
-    let dx = Math.random()*max_speed+0.1;
-    let dy = Math.random()*max_speed+0.1;
-    if(i%2==0) {
-      dx*=-1;
-      dy*=-1;
+  for (let i = 0; i < dots_num; i++) {
+    let x = Math.floor((Math.random() * innerWidth / uni_divs) + (parseInt(i / (dots_num / uni_divs)) * (innerWidth / uni_divs)));
+    let y = Math.floor(Math.random() * innerHeight);
+    let dx = Math.random() * max_speed + 0.1;
+    let dy = Math.random() * max_speed + 0.1;
+    if (i % 2 == 0) {
+      dx *= -1;
+      dy *= -1;
     }
     let temp = new Dot(x, y, dx, dy);
     dots.push(temp);
@@ -82,7 +136,7 @@ function update() {
 
   c.clearRect(0, 0, innerWidth, innerHeight);
 
-  for(let i=0; i<dots_num; i++) {
+  for (let i = 0; i < dots_num; i++) {
 
     let dy = dots[i].dy;
     let dx = dots[i].dx;
@@ -91,10 +145,10 @@ function update() {
     dots[i].y += dy;
 
     // rebounce form walls
-    if(dots[i].x>innerWidth || dots[i].x<0) {
+    if (dots[i].x > innerWidth || dots[i].x < 0) {
       dots[i].dx *= -1;
     }
-    if(dots[i].y>innerHeight || dots[i].y<0) {
+    if (dots[i].y > innerHeight || dots[i].y < 0) {
       dots[i].dy *= -1;
     }
 
@@ -109,9 +163,9 @@ function update() {
     // c.stroke();
 
     // draw its line to mouse
-    let d = Math.sqrt((x-mx)*(x-mx)+(y-my)*(y-my));
-    if(d<mouse_ol) {
-      c.strokeStyle = `rgba(100, 180, 255, ${max_ms_opac*(mouse_ol-d)/mouse_ol})`;
+    let d = Math.sqrt((x - mx) * (x - mx) + (y - my) * (y - my));
+    if (d < mouse_ol) {
+      c.strokeStyle = `rgba(100, 180, 255, ${max_ms_opac * (mouse_ol - d) / mouse_ol})`;
       c.lineWidth = 2;
       c.beginPath();
       c.moveTo(x, y);
@@ -121,17 +175,17 @@ function update() {
 
     // draw lines with other dots
     // for(let i=0; i<dots_num; i++) {
-      for(let j=i+1; j<dots_num; j++) {
-        let x1 = dots[j].x;
-        let y1 = dots[j].y;
-        let d = Math.sqrt((x1-x)*(x1-x)+(y1-y)*(y1-y));
-        if(d<dots_ol) {
-          c.strokeStyle = `rgba(157, 210, 255, ${max_dots_opac*(dots_ol-d)/dots_ol})`;
-          c.lineWidth = 1;
-          c.beginPath();
-          c.moveTo(x1, y1);
-          c.lineTo(x, y);
-          c.stroke();
+    for (let j = i + 1; j < dots_num; j++) {
+      let x1 = dots[j].x;
+      let y1 = dots[j].y;
+      let d = Math.sqrt((x1 - x) * (x1 - x) + (y1 - y) * (y1 - y));
+      if (d < dots_ol) {
+        c.strokeStyle = `rgba(157, 210, 255, ${max_dots_opac * (dots_ol - d) / dots_ol})`;
+        c.lineWidth = 1;
+        c.beginPath();
+        c.moveTo(x1, y1);
+        c.lineTo(x, y);
+        c.stroke();
         // }
       }
     }
