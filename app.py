37720f55b9c83effc8ac955ff609c3f19ade2fcd
gferioli <gferioli@microsoft.com>
2023-09-14 15:51:41 -0400

    added CORS

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, send_from_directory, request
+from flask_cors import CORS, cross_origin
+
 from modules.open_ai import OpenAI
 from modules.message_history import MessageHistory
 import json
@@ -6,12 +8,24 @@ from uuid import uuid4
 from config import Config
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 message_history = MessageHistory()
 
 
 @app.route('/')
 def home():
     return send_from_directory(directory="./static", path="index.html")
+
+def user_response(payload):
+    if payload['prompt'] == "":
+        prompt = payload['user_response']
+    else:
+        prompt = payload['prompt'].replace("{0}", payload['user_response'])
+
+    return prompt
+
 
 
 @app.route('/ask', methods=['POST'])
@@ -20,14 +34,12 @@ def build_model():
     conversation_id = None
     if payload['conversationId'] == "":
         conversation_id = str(uuid4())
-        message_history.append_system_message(conversation_id, payload['system-prompt'])
+        message_history.append_system_message(conversation_id, payload['system_prompt'])
+        if not(payload['user_response'] == ""):
+            message_history.append_user_message(conversation_id, user_response(payload))
     else:
         conversation_id = payload['conversationId']
-        if payload['prompt'] == "":
-            prompt = payload['user-response']
-        else:
-            prompt = payload['prompt'].replace("{0}", payload['user-response'])
-        message_history.append_user_message(conversation_id, prompt)
+        message_history.append_user_message(conversation_id, user_response(payload))
 
     try:
         messages = message_history.get_messages(conversation_id)
