7b346b3cbcad87f87ccc0620039b87ed489f127e
David Berquist <dberquist@microsoft.com>
2023-09-15 15:31:38 -0400

    Adding new open_ai module

---
+++ modules/open_ai2.py
@@ -0,0 +1,14 @@
+import openai
+
+class OpenAI2():
+    def __init__(self, key, model = 'gpt-4'):
+        self.key = key
+        self.model = model
+        openai.api_key = key
+
+    def chat(messageContext = "", requiredWords = [], responses = 3, attempts = 3):
+        for i in range(attempts):
+            allResponses = openai.ChatCompletion.create(model = "gpt-4", messages = messageContext, n = responses).choices
+            for response in allResponses:
+                if all([word in response.message.content for word in requiredWords]):
+                    return response.message.content
