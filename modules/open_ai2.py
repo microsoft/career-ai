0dfac6c5b938d2e34531cb6f0805efc0d778fa01
David Berquist <dberquist@microsoft.com>
2023-09-15 15:40:34 -0400

    Fixing test code

--- modules/open_ai2.py
+++ modules/open_ai2.py
@@ -6,9 +6,9 @@ class OpenAI2():
         self.model = model
         openai.api_key = key
 
-    def chat(messageContext = "", requiredWords = [], responses = 3, attempts = 3):
+    def chat(self, messageContext = "", requiredWords = [], responses = 3, attempts = 3):
         for i in range(attempts):
-            allResponses = openai.ChatCompletion.create(model = "gpt-4", messages = messageContext, n = responses).choices
+            allResponses = openai.ChatCompletion.create(model = self.model, messages = messageContext, n = responses).choices
             for response in allResponses:
                 if all([word in response.message.content for word in requiredWords]):
                     return response.message.content
