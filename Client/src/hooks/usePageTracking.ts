90cd4b43820d285b661aea45293bca3a0df493f7
Won Song <wosong@microsoft.com>
2024-09-16 13:21:35 -0400

    Create CI & CD pipelines

--- Client/src/hooks/usePageTracking.ts
+++ Client/src/hooks/usePageTracking.ts
@@ -1,13 +1,13 @@
 import React from "react";
-import { Telemetry } from "../services/Telemetry";
-import { IAppContext } from "../models/IAppContext";
-import { AppContext } from "../context/AppContext";
+// import { Telemetry } from "../services/Telemetry";
+// import { IAppContext } from "../models/IAppContext";
+// import { AppContext } from "../context/AppContext";
 
 export function usePageTracking() {
   // const { currentScreen } = React.useContext<IAppContext>(AppContext);
 
   React.useEffect(() => {
-    const telemetry = Telemetry.getInstance();
+    // const telemetry = Telemetry.getInstance();
     // telemetry.trackPageView(window.location.pathname);
 
     return () => {
