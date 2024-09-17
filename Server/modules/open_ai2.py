eb778d70190726284e75307773ad25a2a17ee03c
Carlos Rodriguez <carodriguez@microsoft.com>
2024-09-17 16:30:47 -0400

    Modified default value on response number

--- Server/modules/open_ai2.py
+++ Server/modules/open_ai2.py
@@ -8,7 +8,7 @@ class OpenAI2():
 
         openai.api_key = key
 
-    def chat(self, messageContext = "", requiredWords = [], number_of_responses = 1, attempts = 3):
+    def chat(self, messageContext = "", requiredWords = [], number_of_responses = 10, attempts = 3):
         for i in range(attempts):
             try:
                 allResponses = openai.ChatCompletion.create(
