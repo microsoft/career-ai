02c482d3707afe4c3e9462db95895d1348222260
anpethel <anpethel@microsoft.com>
2024-09-17 11:03:53 -0400

    adding header logo with scss class

--- Client/src/components/Layout/Header/Header.tsx
+++ Client/src/components/Layout/Header/Header.tsx
@@ -5,7 +5,13 @@ export function Header(): React.ReactElement {
   return (
     <header className="header-root">
       <a href="/" title="Go to CareerCraft home page" className="brand-link">
-        <div className="brand">CareerCraft</div>
+        <div className="brand">
+          <img
+            src="Flat_logo_linear.png"
+            alt="CareerCraft logo"
+            className="header-logo"
+          />
+        </div>
       </a>
     </header>
   );
