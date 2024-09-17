d6f1cd6328e6612320933991bd4261391fd7f243
anpethel <anpethel@microsoft.com>
2024-09-17 12:43:15 -0400

    adding margin to header

--- Client/src/components/Layout/Header/Header.tsx
+++ Client/src/components/Layout/Header/Header.tsx
@@ -8,6 +8,7 @@ export function Header(): React.ReactElement {
         <div className="brand">
           <img
             src="Flat_logo_linear.png"
+            // src="CareerCraft_logo_white_no_border_with_slogan_large.png"
             alt="CareerCraft logo"
             className="header-logo"
           />
