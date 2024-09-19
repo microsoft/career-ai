fb383224c8ac2210303739c0fa4a62c87e14fca4
gferioli0418 <gferioli0418@gmail.com>
2024-09-19 14:29:58 -0400

    Gferioli/migrate to azure openai (#45)
    * update azure openai and add isntructor support
    
    * add instructor class to format the gpt40 responses
    
    * add package-lock.json
    
    * add setting.json to gitignore
    
    * update to openai.py
    
    * update rounds class
    
    * update openai to use azure openai
    
    * update to 3 options onls
    
    * remove package.json from being tracked
    
    * do not limit word usage
    
    * add validate api
    
    * add extra api to validate code
    
    * change max_tokens

--- Server/modules/career_game.py
+++ Server/modules/career_game.py
@@ -3,6 +3,8 @@ import json
 from uuid import uuid4
 from time import time
 
+from pydantic import BaseModel, Field
+
 
 class CareerGame:
     def __init__(self, game_prompts, message_history, open_ai, telemetry, logger):
@@ -21,15 +23,17 @@ class CareerGame:
 
     def start_game(self, career_choice):
         self.logger.debug(
-            "Starting new game with career choice: {}".format(career_choice))
+            "Starting new game with career choice: {}".format(career_choice)
+        )
         conversation_id = str(uuid4())
-        self.logger.debug("NewGameStarted", {
-            "conversationId": conversation_id
-        })
+        self.logger.debug("NewGameStarted", {"conversationId": conversation_id})
         self.message_history.append_system_message(
-            conversation_id, self.game_prompts["preGamePrompt"])
+            conversation_id, self.game_prompts["preGamePrompt"]
+        )
         self.message_history.append_user_message(
-            conversation_id, self.game_prompts["userResponsePrompt"].format(career_choice))
+            conversation_id,
+            self.game_prompts["userResponsePrompt"].format(career_choice),
+        )
 
         # self.logger.debug("message history: {}".format(self.message_history))
 
@@ -39,41 +43,82 @@ class CareerGame:
         # self.logger.debug(
         #     "Continuing game with user choice: {}".format(user_choice))
         self.message_history.append_user_message(
-            conversation_id, self.game_prompts["userResponsePrompt"].format(user_choice))
+            conversation_id, self.game_prompts["userResponsePrompt"].format(user_choice)
+        )
 
         return self.__process_game__(conversation_id)
 
     def complete_game(self, conversation_id, user_choice):
         self.message_history.append_user_message(
-            conversation_id, self.game_prompts["postGamePrompt"].format(user_choice))
+            conversation_id, self.game_prompts["postGamePrompt"].format(user_choice)
+        )
 
         return self.__process_game__(conversation_id)
 
     def __process_game__(self, conversation_id):
         messages = self.message_history.get_messages(conversation_id)
-        # self.logger.debug("Processing game with messages: {}".format(messages))
         response = None
 
         while response is None:
             try:
-                # This seems to sometimes generate 10 scenarios or 1.... need to make the result consistent
-                open_ai_response = self.open_ai.chat(messages)
+                open_ai_response = self.open_ai.make_request(
+                    messages, response_model=Rounds, retries=1
+                )
+                print("open_ai_response: ", open_ai_response)
                 if open_ai_response is None:
                     continue
 
-                response = open_ai_response
+                response = json.loads(open_ai_response.model_dump_json())
+                print(f"response: {response}")
             except Exception as e:
                 # Can we give it a max retries?
 
-                self.telemetry.debug("RetryingOpenAIChat", {
-                    "conversationId": conversation_id,
-                    "error": str(e)
-                })
+                self.telemetry.debug(
+                    "RetryingOpenAIChat",
+                    {"conversationId": conversation_id, "error": str(e)},
+                )
                 time.sleep(5)
 
-        self.message_history.append_assistant_message(conversation_id, json.dumps(response))
+        self.message_history.append_assistant_message(
+            conversation_id, json.dumps(response)
+        )
 
-        return {
-            "conversationId": conversation_id,
-            "round": response
-        }
+        return {"conversationId": conversation_id, "round": response}
+
+    def validate(self, user_choice):
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "validate if the following career choice is valid: "
+                        + user_choice,
+                    }
+                ],
+            },
+        ]
+        open_ai_response = self.open_ai.make_request(
+            messages=messages, response_model=ValidCareerChoice, retries=3
+        )
+        response = json.loads(open_ai_response.model_dump_json())
+        print(f"response: {response}")
+
+        return response
+
+
+class Rounds(BaseModel):
+    outcome: str = Field(
+        description="The outcome of the previous scenario and some background information if necessary"
+    )
+    scenario: str = Field(description="The scenario that the user is in.")
+    options: list[str] = Field(
+        description="The 3 options the user has to choose from. Do not include list formatting just add the string."
+    )
+
+
+class ValidCareerChoice(BaseModel):
+    isValid: bool = Field(
+        description="Whether the career choice is valid or not. NSFW content is not valid."
+    )
