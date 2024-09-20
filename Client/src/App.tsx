b1f1fba40b89ea8a52ef0120b9338a1a7c5a2d4b
Won Song <wosong@microsoft.com>
2024-09-20 16:52:01 -0400

    Remove admin pages

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -5,7 +5,6 @@ import { StartScreen } from "./screens/StartScreen";
 import { Route, Routes } from "react-router-dom";
 import { GameScreen } from "./screens/GameScreen";
 import { ResultScreen } from "./screens/ResultScreen";
-import { AdminScreen } from "./screens/AdminScreen/AdminScreen";
 import { AboutScreen } from "./screens/AboutScreen/AboutScreen";
 import { PartnersScreen } from "./screens/PartnersScreen/PartnersScreen";
 import { ContactUsScreen } from "./screens/ContactUsScreen/ContactUsScreen";
@@ -18,11 +17,8 @@ function App() {
         <Route path="/" element={<StartScreen />} />
         <Route path="/career-game" element={<GameScreen />} />
         <Route path="/howToPlay" element={<HowToPlayScreen />} />
-        <Route
-          path="/career-game/results"
-          element={<ResultScreen />}
-        />
-        <Route path="/admin" element={<AdminScreen />} />
+        <Route path="/career-game/results" element={<ResultScreen />} />
+        {/* <Route path="/admin" element={<AdminScreen />} /> */}
         <Route path="/about" element={<AboutScreen />} />
         <Route path="/partners" element={<PartnersScreen />} />
         <Route path="/contact" element={<ContactUsScreen />} />
